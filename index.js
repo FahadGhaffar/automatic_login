6f0b3abf7d6a48625a3b7ccc025072e42e112c18
ayesha ghaffar <gayesha759@gmail.com>
2024-09-13 05:20:09 +0500

    update

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-const { Keyboard } = require('puppeteer');
-const puppeteer = require('puppeteer-extra');
-const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+// const { Keyboard } = require('puppeteer');
+// const puppeteer = require('puppeteer-extra');
+// const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 
-puppeteer.use(StealthPlugin());
+// puppeteer.use(StealthPlugin());
 
-const googleUsername = "fahad.ghafar.dg@gmail.com";
-const googlePassword = "Jawwad54321@";
-function delay(time) {
-    return new Promise(resolve => setTimeout(resolve, time));
-}
+// const googleUsername = "fahad.ghafar.dg@gmail.com";
+// const googlePassword = "Jawwad54321@";
+const puppeteer = require('puppeteer');
+
 (async () => {
     const browser = await puppeteer.launch({
         headless: false,
@@ -20,41 +19,45 @@ function delay(time) {
         ]
     });
 
-    const loginUrl = "https://soundcloud.com/signin";
-    // const loginUrl = "https://accounts.google.com/AccountChooser?service=mail&continue=https://google.com&hl=en";
-    const ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Mobile Safari/537.36';
+    const loginUrl = 'https://soundcloud.com/login';
+    const username = "fahad.ghaffar.office@gmail.com";
+    const password = "Jawwad54321@";
+
     const page = await browser.newPage();
-
-
-    await page.setUserAgent(ua);
-    await page.setExtraHTTPHeaders({
-        'Accept-Language': 'en-US,en;q=0.9',
-        'Referer': 'https://accounts.google.com/',
-    });
-
-    // await page.goto(loginUrl, { waitUntil: 'networkidle2' });
-    // await page.type('input[type="email"]', googleUsername);
-    // await page.keyboard.press('Enter');
-    // // await page.waitForTimeout(2000);
-    await delay(3000);
-    // await page.type('input[type="password"]', googlePassword);
-    // await page.keyboard.press('Enter');
-
-    // await page.goto(loginUrl);
     await page.goto(loginUrl, { waitUntil: 'networkidle2' });
-    const allElements = await page.$$('*');
-   console.log(`Total elements on the page: ${allElements}`);
-    await page.waitForSelector('input[name="email"][type="text"]');
-    await delay(10000);
-    await page.type('input[name="email"][type="text"]', googleUsername);
-    await delay(2000);
-    await page.keyboard.press('Enter');
 
-    await page.waitForSelector('input[type="password"]');
-    await page.type('input[type="password"]', googlePassword);
-    await page.keyboard.press('Enter');
+    await page.click('button[id="onetrust-reject-all-handler"]', { delay: 1000 });
+    // await page.waitForXPath('//*[@id="sign_in_up_email"]');
+    // const usernameInput = await page.$x('//*[@id="sign_in_up_email"]');
+    // await usernameInput[0].type(username, { delay: 100 });
+    const frame = await page.$('/html/body/div[3]/iframe');
+    // const frame = await page.frames().find(f => f.src() === 'https://secure.soundcloud.com/web-auth?client_id=T9frtmUVGsxBucS3jqGV9A8lsB3MAO47&device_id=398602-224456-198272-585932&app_id=46941&tracking=local&redirect_uri=https%3A%2F%2Fsoundcloud.com%2Fsignin%2Fcallback&state=eyJjbGllbnRfaWQiOiJUOWZydG1VVkdzeEJ1Y1MzanFHVjlBOGxzQjNNQU80NyIsIm5vbmNlIjoiUm9qbGk3SDc5Y25ZYlN1U1p0SkpicmR0c0ttNkt6a1dvUXRlcGNwbHhtZTJLQXpFX1p1M0dQbXNzZ2NET3hxcSJ9&code_challenge=8p1ySQzDC2uP3wn-bLDtUrCc02czu9iUXCMyZwiiS_E&code_challenge_method=S256#page_name=signin%3Amain&page_urn=soundcloud%3Asignin%3Amain&start_url=https%3A%2F%2Fsoundcloud.com%2Fsignin&start_view=sign_in&is_visible=true');
+    // console.log(frame);
+    // Log the names and URLs of all frames
+
+    // await frame.click('input[id="sign_in_up_email"]', { delay: 100 });
+    await frame.waitForSelector('input[id="sign_in_up_email"]', { delay: 500 });
+    // await page.$eval('input[type="text"][name="email]', el => el.value = 'fahad.ghaffar.office@gmail.com');
+    await frame.type('input[id="sign_in_up_email"]', username, { delay: 100 });
+
+
+    await page.waitForSelector('input[name="password"]');
+    await page.type('input[name="password"]', password, { delay: 100 });
+
+    await page.click('button[type="submit"]');
+
+    // Wait for the login to complete
+    await page.waitForNavigation({ waitUntil: 'networkidle2' });
+
+    // You're now logged in!
+    console.log('Logged in to SoundCloud!');
+
+    // Do something with the logged-in page
+    await page.goto('https://soundcloud.com/your_username');
+
+    // Close the browser
+    await browser.close();
 })();
-
 // const puppeteer = require('puppeteer');
 // const fs = require('fs');
 
